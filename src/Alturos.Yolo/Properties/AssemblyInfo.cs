b15961b34e8f4794c11a7a3e16f465a040bbfe68
Tino Hager <tino.hager@alturos.com>
2018-06-22 16:41:06 +0200

    optimize nuget package info

--- src/Alturos.Yolo/Properties/AssemblyInfo.cs
+++ src/Alturos.Yolo/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Alturos.Yolo")]
-[assembly: AssemblyDescription("Yolo Darknet Wrapper")]
+[assembly: AssemblyDescription("Yolo Darknet Wrapper, Real-Time Object Detection")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alturos Destinations")]
 [assembly: AssemblyProduct("Alturos.Yolo")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
