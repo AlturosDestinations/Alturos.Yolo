b51ce1da1ba047b6c6f13088cc3bd0cb0188b505
Tino Hager <tino.hager@nager.at>
2018-07-10 22:34:17 +0200

    Increase version

--- src/Alturos.Yolo/Properties/AssemblyInfo.cs
+++ src/Alturos.Yolo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Alturos.Yolo")]
-[assembly: AssemblyDescription("Yolo Darknet Wrapper, Real-Time Object Detection")]
+[assembly: AssemblyDescription("Yolo Darknet Wrapper, Real-Time Object Detection (yolo core of AlexeyAB/darknet)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alturos Destinations")]
 [assembly: AssemblyProduct("Alturos.Yolo")]
-[assembly: AssemblyCopyright("Copyright ©  2018 Alturos Destinations")]
+[assembly: AssemblyCopyright("Copyright © 2018 Alturos Destinations")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
