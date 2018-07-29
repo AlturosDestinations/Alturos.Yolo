fb572929a87af54eee15e7e7d72187d4e4ccc4a4
Tino Hager <tino.hager@nager.at>
2018-07-29 20:12:14 +0200

    Add UnitTest project

---
+++ src/Alturos.Yolo.UnitTest/BasicTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace Alturos.Yolo.UnitTest
+{
+    [TestClass]
+    public class BasicTest
+    {
+        private string _imagePath = @"..\..\..\..\Images\Motorbike1.jpg";
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void FileNotFoundTest()
+        {
+            var configuration = new ConfigurationDetector().Detect();
+            using (var yoloWrapper = new YoloWrapper(configuration))
+            {
+                yoloWrapper.Detect("image-not-exists.jpg");
+            }
+        }
+
+        [TestMethod]
+        public void DetectFromFilePath()
+        {
+            var configuration = new ConfigurationDetector().Detect();
+            using (var yoloWrapper = new YoloWrapper(configuration))
+            {
+                var items = yoloWrapper.Detect(this._imagePath);
+                Assert.IsTrue(items.Count() > 0);
+            }
+        }
+
+        [TestMethod]
+        public void DetectFromFileData()
+        {
+            var configuration = new ConfigurationDetector().Detect();
+            using (var yoloWrapper = new YoloWrapper(configuration))
+            {
+                var imageData = File.ReadAllBytes(this._imagePath);
+                var items = yoloWrapper.Detect(imageData);
+                Assert.IsTrue(items.Count() > 0);
+            }
+        }
+    }
+}
---
+++ src/Alturos.Yolo.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Alturos.Yolo.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Alturos.Yolo.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("850159d7-0908-41d8-a8ba-e0e44fc4dd36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
