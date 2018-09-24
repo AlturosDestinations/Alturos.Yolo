2b8e402844590cf1550613fe8a956011580a255a
Tino Hager <tino.hager@nager.at>
2018-09-24 22:22:48 +0200

    Optimize configuration detector

--- src/Alturos.Yolo/ConfigurationDetector.cs
+++ src/Alturos.Yolo/ConfigurationDetector.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 
 namespace Alturos.Yolo
 {
     public class ConfigurationDetector
     {
-        public YoloConfiguration Detect()
+        public YoloConfiguration Detect(string path = ".")
         {
-            var files = this.GetYoloFiles();
+            var files = this.GetYoloFiles(path);
             var yoloConfiguration = this.MapFiles(files);
             var configValid = this.AreValidYoloFiles(yoloConfiguration);
 
@@ -20,9 +19,9 @@ namespace Alturos.Yolo
             throw new FileNotFoundException("Cannot found pre-trained model, check all config files available (.cfg, .weights, .names)");
         }
 
-        private string[] GetYoloFiles()
+        private string[] GetYoloFiles(string path)
         {
-            return Directory.GetFiles(".", "*.*", SearchOption.TopDirectoryOnly).Where(o => o.EndsWith(".names") || o.EndsWith(".cfg") || o.EndsWith(".weights")).ToArray();
+            return Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly).Where(o => o.EndsWith(".names") || o.EndsWith(".cfg") || o.EndsWith(".weights")).ToArray();
         }
 
         private YoloConfiguration MapFiles(string[] files)
@@ -39,11 +38,6 @@ namespace Alturos.Yolo
             if (string.IsNullOrEmpty(config.ConfigFile) ||
                 string.IsNullOrEmpty(config.WeightsFile) ||
                 string.IsNullOrEmpty(config.NamesFile))
-            {
-                return false;
-            }
-
-            if (Path.GetFileNameWithoutExtension(config.ConfigFile) != Path.GetFileNameWithoutExtension(config.WeightsFile))
             {
                 return false;
             }
